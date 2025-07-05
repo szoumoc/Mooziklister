09ddf70dbb2273d2a61637d52feef79a8991eeea
szoumoc <soumosaha4327@gmail.com>
2025-07-06 02:06:35 +0530

    Modified Models

--- src/appresponse/models.py
+++ src/appresponse/models.py
@@ -6,9 +6,10 @@ import uuid
 class AppResponse(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     session_id = models.UUIDField(db_index=True, default=uuid.uuid4)
-    user_input = models.TextField()
-    llm_response = models.JSONField()
+    user_input = models.TextField(default=list, help_text="User input text")
+    llm_responses = models.JSONField(default=list, help_text="List of LLM response objects")
     confirmed = models.BooleanField(default=False)
+    user_feedback = models.BooleanField(default=True, help_text="User feedback on the response")
 
 
     def __str__(self):
--- src/user/models.py
+++ src/user/models.py
@@ -7,7 +7,10 @@ class BaseModel(models.Model):
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
     created = models.DateTimeField(auto_now_add=True)
-    foreign_key_to_playlist = models.CharField(max_length=12, blank=True, null=True)
+    spotify_id = models.CharField(max_length=30, unique=True, blank=False, null=True, help_text="Spotify ID of the user")
+    foreign_key_to_playlist = models.CharField(max_length=12, blank=True, null=True, default=list, help_text="Foreign key to playlist, stored as a string")
     
+  
+  
     def __str__(self):
         return str(self.id)
