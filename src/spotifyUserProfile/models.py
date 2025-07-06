d3ed4a05b9a16380b8467fc569ee6236f1736614
szoumoc <soumosaha4327@gmail.com>
2025-07-06 13:48:17 +0530

    Updated Model added and renamed tables

--- src/spotifyUserProfile/models.py
+++ src/spotifyUserProfile/models.py
@@ -14,12 +14,14 @@ class SpotifyUserProfile(models.Model):
         return f"{self.user.username} - {self.spotify_user_id}"
     
 
-class Playlist(models.Model):
+class Create_Playlist(models.Model):
     id = models.AutoField(primary_key=True)
+    spotify_user_id = models.ForeignKey(SpotifyUserProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     public = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    playlist_uri = models.CharField(max_length=255, unique=True, blank=True, null=True)  # Spotify URI for the playlist
 
     def __str__(self):
         return self.name
@@ -28,8 +30,8 @@ class Playlist(models.Model):
 class PlaylistSpotifyResponse(models.Model):
     pass
 
-class PlaylistTrack(models.Model):
-    playlist_id = models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name='tracks') #stores the playlist this track belongs to
+class add_these_tracks(models.Model):
+    playlist_id = models.ForeignKey(Create_Playlist, on_delete=models.CASCADE, related_name='tracks') #stores the playlist this track belongs to
     position = models.IntegerField(blank=True, null=True)  # i might not need this
     uris = models.JSONField()  # Stores list of track/episode URIs
     created_at = models.DateTimeField(auto_now_add=True)
@@ -40,13 +42,31 @@ class PlaylistTrack(models.Model):
 
 class PlaylistImage(models.Model):
 
-    playlist_id = models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name='images') #the playlist, image belongs to
+    playlist_id = models.ForeignKey(Create_Playlist, on_delete=models.CASCADE, related_name='images') #the playlist, image belongs to
     image = models.ImageField(upload_to='playlist_images/')
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"Image for {self.playlist_id.name}"
     
+
+class TrackQuery(models.Model):
+    query = models.CharField(max_length=255)
+    type = "track"
+    market = "ES"
+    limit = 1
+    offset = 0
+    include_external = "audio"
+
+    def __str__(self):
+        return f"{self.query}"
+    
+class get_track(models.Model):
+    track_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Track ID: {self.track_id}"
+
 
 
 
